90e5802b8a91ed3abdf90d4474da8e2f6735211c
Sơn <88760761+ahihison@users.noreply.github.com>
2023-11-21 20:21:27 +0700

    test: commit

--- notion-clone/app/page.tsx
+++ notion-clone/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
-  return ( 
-   <p className="text-red-500">Hello Notion
-   <Button>Click me</Button></p>
+  return (
+    <p className="text-red-500">Hello Notion
+      <Button>Click me   </Button></p>
   )
 }
