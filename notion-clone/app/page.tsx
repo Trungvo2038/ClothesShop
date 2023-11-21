ba387b1e48a2f1a3e1a06eedc2a80e0de9dd6f04
Sơn <88760761+ahihison@users.noreply.github.com>
2023-11-21 20:09:30 +0700

    feat: init project

---
+++ notion-clone/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return ( 
+   <p className="text-red-500">Hello Notion
+   <Button>Click me</Button></p>
+  )
+}
